9cdde042a05ff98c6689e1f4530bcc85dcd98f00
Ehsan <ehsan.mehralian@gmail.com>
2017-03-05 01:28:08 +0330

    homeWork2 :(

---
+++ HW2/grayCodeCounter.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/04/2017 10:57:41 AM
+-- Design Name: 
+-- Module Name: grayCodeCounter - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity grayCodeCounter is
+--  Port ( );
+end grayCodeCounter;
+
+architecture Behavioral of grayCodeCounter is
+
+begin
+
+
+end Behavioral;

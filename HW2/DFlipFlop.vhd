f4c6c7c30b19bd26a9919cde93a45783af51ea89
Ehsan <ehsan.mehralian@gmail.com>
2017-03-05 13:36:04 +0330

    ripple counter implimeted

--- HW2/DFlipFlop.vhd
+++ HW2/DFlipFlop.vhd
@@ -31,34 +31,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity TFlipFlop is
+entity DFlipFlop is
 port
   (
     clk: in std_logic;
-    T: in std_logic;
+    D: in std_logic;
     reset : in std_logic;
     output: out std_logic;
     output_not: out std_logic);
 
-end TFlipFlop;
+end DFlipFlop;
 
-architecture Behavioral of TFlipFlop is
+architecture Behavioral of DFlipFlop is
 signal output_buf:std_logic := '0';
 begin
-output <= output_buf;
+    output <= output_buf;
     output_not <= not output_buf;
     
     process(clk,reset)
-      begin
-        if(reset = '0') then
+    begin
+        if(reset = '1') then
           output_buf <= '0';
-        elsif(clk'event and clk = '1') then
-         if (T='1')then
-          output_buf <= not output_buf  ;
-         end if;
+        elsif (rising_edge(clk)) then
+            output_buf <= D;
         end if;
-      end process;
 
-
-
+    end process;
 end Behavioral;
--- HW2/rippleCounter.vhd
+++ HW2/rippleCounter.vhd
@@ -32,26 +32,31 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity rippleCounter is
-  --  generic (n : integer);
+
+    generic (n : natural := 4);
     Port ( clk: in std_logic;
            reset: in std_logic;
-           o: out std_logic_vector(3 downto 0)
+           o: out std_logic_vector(n-1 downto 0)
     );
 end rippleCounter;
 
 architecture RTL of rippleCounter is
-    component TFlipFlop is
+    component DFlipFlop is
     port(
       clk: in std_logic;
-       T: in std_logic;
+       D: in std_logic;
       reset : in std_logic;
       output: out std_logic;
       output_not: out std_logic);
     end component;
-   signal o_not: std_logic_vector(3 downto 0);
+   signal i_thClk, i_thOutNot: std_logic_vector(n-1 downto 0);
+   
    begin
-     tff_ins0: TFlipFlop port map (clk, '1', reset, o(0), o_not(0));
-     tff_ins1: TFlipFlop port map (o_not(0), '1', reset, o(1), o_not(1));
-     tff_ins2: TFlipFlop port map (o_not(1), '1', reset, o(2), o_not(2));
-     tff_ins3: TFlipFlop port map (o_not(2), '1', reset, o(3), o_not(3));
+   i_thClk(0) <= clk;
+   i_thClk(n-1 downto 1) <= i_thOutNot(n-2 downto 0);
+   
+   gen_cnt: for i in 0 to n-1 generate
+      dff_i:DFlipFlop port map (i_thClk(i),i_thOutNot(i),reset,o(i),i_thOutNot(i));
+   end generate;
+
 end RTL;

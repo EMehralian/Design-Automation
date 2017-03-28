9e6fd24d78fb784fd360923c51784b24ce56febe
Ehsan <ehsan.mehralian@gmail.com>
2017-03-28 15:59:35 +0430

    up/down 5 bit counter

--- HW3/updown5bitCounter.vhd
+++ HW3/updown5bitCounter.vhd
@@ -21,7 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -33,13 +33,36 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity updown5bitCounter is
 port(
-
+        dir,rst,clk: in std_logic;
+        y: out std_logic_vector(4 downto 0)
 );
 end updown5bitCounter;
 
 architecture Behavioral of updown5bitCounter is
-
+    signal count: std_logic_vector(4 downto 0):= (others =>'0');
 begin
+    process(clk , dir)
+    begin
+        if(rst = '1') then
+            count <= (others => '0');
+        elsif( clk'event and clk ='1') then
+            if(dir = '0') then
+                if (count = "10010") then
+                    count <= (others => '0');
+                else
+                    count <= count + '1';
+                end if;              
+            elsif ( dir = '1') then
+                if count = "00000" then
+                    count <= "10010";
+                else
+                    count <= count - '1';
+                end if;     
+            end if;
+         end if;
+    end process;
+    y <= count;
+
 
 
 end Behavioral;

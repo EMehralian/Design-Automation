e85cdf3d540f4d246dc69edb27eb863cccd4ac9b
Ehsan <ehsan.mehralian@gmail.com>
2017-05-26 22:52:06 +0430

    each Round

--- HW6/Round.vhd
+++ HW6/Round.vhd
@@ -32,12 +32,32 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Round is
---  Port ( );
+port(
+    input1, input2: in std_logic_vector(7 downto 0);
+    key: in std_logic_vector(7 downto 0);
+    out1,out2: out std_logic_vector(7 downto 0)
+    );
 end Round;
 
 architecture Behavioral of Round is
-
+component adder is  
+  port(A,B : in std_logic_vector(7 downto 0);  
+      SUM : out std_logic_vector(7 downto 0));  
+end component adder;
+component xorer is  
+  port(A,B : in std_logic_vector(7 downto 0);  
+      output : out std_logic_vector(7 downto 0));  
+end component xorer; 
+signal sumSig,xorSig,shiftSig: std_logic_vector (7 downto 0);
+signal sumSig2,xorSig2,shiftSig2: std_logic_vector (7 downto 0);
 begin
+inst0: xorer port map(input1,input2,xorSig);
+shiftSig <= xorSig(6 downto 0) & xorSig(7);
+inst1: adder port map(shiftSig, key , sumSig);
 
-
+inst2: xorer port map(sumSig,input2,xorSig2);
+shiftSig2 <= xorSig2(6 downto 0) & xorSig2(7);
+inst3: adder port map(shiftSig2, key , sumSig2);
+out1<= sumSig;
+out2 <= sumSig2;
 end Behavioral;

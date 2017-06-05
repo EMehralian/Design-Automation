040e788b4696672695cbaf2e89f8dc817e2b10fc
Ehsan <ehsan.mehralian@gmail.com>
2017-06-05 05:33:24 +0430

    base of non-restoring divider

--- Project3/binery8to4divider.vhd
+++ Project3/binery8to4divider.vhd
@@ -33,14 +33,62 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity binery8to4divider is
 port(
-    A,Q,B:in std_logic_vector(3 downto 0);
+    clk: in std_logic;
+    A2,A1,B:in std_logic_vector(3 downto 0);
     ov: out std_logic
 );
 end binery8to4divider;
 
 architecture Behavioral of binery8to4divider is
 
+component xorer is  
+  port(A,B : in std_logic_vector(7 downto 0);  
+      output : out std_logic_vector(7 downto 0));  
+end component;
+
+component addsub is
+   port( OP: in std_logic;
+          A,B  : in std_logic_vector(3 downto 0);
+          output  : out std_logic_vector(3 downto 0);
+          Cout: out std_logic);
+end component; 
+
+signal SC: integer := 4;
+signal sub,sum: std_logic_vector(3 downto 0);
+signal A: std_logic_vector(3 downto 0):=A2;
+signal Q: std_logic_vector(3 downto 0):=A1;
+signal c,c1,c2: std_logic;
 begin
-
-
+    inst0: addsub port map('1',A,B,sub,c1);
+    inst1: addsub port map('0',A,B,sum,c2);
+    process(clk)
+        begin   
+            if rising_edge(clk) then
+                A <= sub;
+                if (c1 = '1') then
+                    A <= sum;
+                    ov<='1';
+                else
+                    A<= sum;
+                    for I in 0 to SC loop
+                        c<= A(3);
+                        A(3 downto 1) <= A(2 downto 0) & Q(3);
+                        Q(3 downto 1) <= Q( 3 downto 1) & '0';
+                        if (c = '1') then
+                            Q(0)<='1';
+                            A<= sub;
+                        else
+                            A<=sub;
+                            if(c1 = '1') then
+                                Q(0)<='1';
+                                
+                            else
+                                Q(0)<='0';
+                                A<=sum;
+                            end if;
+                        end if;
+                    end loop;
+                end if;
+            end if;
+        end process;
 end Behavioral;

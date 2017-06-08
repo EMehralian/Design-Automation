21fbc2d2463f30bcac54b7c71a20c35525f2e7c0
Ehsan <ehsan.mehralian@gmail.com>
2017-06-08 06:26:03 +0430

    non restoring divider 8 bit to 4 bit

--- Project3/binery8to4divider.vhd
+++ Project3/binery8to4divider.vhd
@@ -21,7 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use ieee.numeric_std;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -34,8 +34,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity binery8to4divider is
 port(
     clk: in std_logic;
-    A2,A1,B:in std_logic_vector(3 downto 0);
-    ov: out std_logic
+    A:in std_logic_vector(3 downto 0):="0000";
+    Q:in std_logic_vector(3 downto 0):="1100";
+    B:in std_logic_vector(3 downto 0):="0010";
+    ov: out std_logic;
+    F,D:out std_logic_vector(3 downto 0)
 );
 end binery8to4divider;
 
@@ -53,42 +56,61 @@ component addsub is
           Cout: out std_logic);
 end component; 
 
-signal SC: integer := 4;
-signal sub,sum: std_logic_vector(3 downto 0);
-signal A: std_logic_vector(3 downto 0):=A2;
-signal Q: std_logic_vector(3 downto 0):=A1;
-signal c,c1,c2: std_logic;
+signal SC: integer := 3;
+signal sub,sub1,sum,sum1,sum2: std_logic_vector(3 downto 0);
+signal temp1 : std_logic_vector(3 downto 0):=A;
+signal temp2 : std_logic_vector(3 downto 0):=Q;
+signal c,c1,c2,c3,c4: std_logic;
 begin
-    inst0: addsub port map('1',A,B,sub,c1);
-    inst1: addsub port map('0',A,B,sum,c2);
+
+    inst0: addsub port map('1',temp1,B,sub,c1);
+    inst1: addsub port map('0',temp1,B,sum,c2);
+ --   sum1 <=  sum(2 downto 0) & Q(3);
+    inst2: addsub port map('0',sub1,B,sum2,c3);
+    inst3: addsub port map('1',temp1(2 downto 0) & temp2(3),B,sub1,c3);
     process(clk)
-        begin   
+        variable varA: std_logic_vector(3 downto 0);
+        variable varQ: std_logic_vector(3 downto 0);
+        variable varC:std_logic;
+        variable flag: boolean:=True;
+        begin  
             if rising_edge(clk) then
-                A <= sub;
-                if (c1 = '1') then
-                    A <= sum;
-                    ov<='1';
+                if flag then
+                    flag := False;
+                    varA := sub;
+                    if (c1 = '1') then
+                        varA := sum;
+                        ov<='1';
+                    else
+                        varA := sum;
+                        ov<= '0';
+                    end if;
+                    varQ := Q;
+                    varA := A;
                 else
-                    A<= sum;
-                    for I in 0 to SC loop
-                        c<= A(3);
-                        A(3 downto 1) <= A(2 downto 0) & Q(3);
-                        Q(3 downto 1) <= Q( 3 downto 1) & '0';
-                        if (c = '1') then
-                            Q(0)<='1';
-                            A<= sub;
+                    if SC > 0 then
+                        varC:= varA(3);
+                        varA := temp1(2 downto 0) & temp2(3);
+                        varQ := temp2(2 downto 0) & '0';
+                        if (varC = '1') then
+                            varQ(0):='1';
+                            varA:= sub1;
                         else
-                            A<=sub;
-                            if(c1 = '1') then
-                                Q(0)<='1';
-                                
+                            varA:=sub1;
+                            if(c = '1') then
+                                varQ(0):='1';          
                             else
-                                Q(0)<='0';
-                                A<=sum;
+                                varQ(0):='0';
+                                varA:=sum2;
                             end if;
-                        end if;
-                    end loop;
+                        end if; 
+                        SC <= SC - 1;
+
+                    end if;         
                 end if;
+
             end if;
+            temp1 <= varA;
+            temp2 <= varQ;
         end process;
 end Behavioral;
